1d93eec43c686131c75adda44283f577e82f9de0
kannika07112 <kkannikarao07@gmail.com>
2026-01-14 12:59:08 +0530

    Question 15

---
+++ Basic-for-training/Conditionla-basicReasoning-1-20/15-VotingEligibility.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    if (age >= 18)
+        printf("Eligible for voting");
+    else
+        printf("Not eligible for voting");
+
+    return 0;
+}
