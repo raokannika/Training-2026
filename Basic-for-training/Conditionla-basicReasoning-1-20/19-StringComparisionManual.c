b97f2b2b80384713c387ea8763f0d7dafd0bc9cc
kannika07112 <kkannikarao07@gmail.com>
2026-01-14 13:00:28 +0530

    Question 19

---
+++ Basic-for-training/Conditionla-basicReasoning-1-20/19-StringComparisionManual.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char str1[50], str2[50];
+    int i = 0, flag = 0;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    while (str1[i] != '\0' || str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            flag = 1;
+            break;
+        }
+        i++;
+    }
+
+    if (flag == 0)
+        printf("Strings are equal");
+    else
+        printf("Strings are not equal");
+
+    return 0;
+}
