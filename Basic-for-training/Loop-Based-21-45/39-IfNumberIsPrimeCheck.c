6a5b79b8cdcb86e4fa151e6f56b6ef628ad7df79
kannika07112 <kkannikarao07@gmail.com>
2026-01-15 20:36:12 +0530

    Question 39

---
+++ Basic-for-training/Loop-Based-21-45/39-IfNumberIsPrimeCheck.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, flag = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0 && n > 1)
+        printf("Prime number");
+    else
+        printf("Not a prime number");
+
+    return 0;
+}
