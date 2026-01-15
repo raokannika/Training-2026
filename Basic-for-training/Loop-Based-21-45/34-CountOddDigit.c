a7d3dd2545d6f006334cf2d999fe4605d761ef96
kannika07112 <kkannikarao07@gmail.com>
2026-01-15 20:35:10 +0530

    Question 34

---
+++ Basic-for-training/Loop-Based-21-45/34-CountOddDigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        if ((num % 10) % 2 != 0)
+            count++;
+        num /= 10;
+    }
+
+    printf("Odd digits count = %d", count);
+    return 0;
+}
