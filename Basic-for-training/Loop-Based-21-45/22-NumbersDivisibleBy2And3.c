e14fb069045ebffcbd5ea565d7e49f0e9f07cb83
kannika07112 <kkannikarao07@gmail.com>
2026-01-15 15:27:27 +0530

    Question 22

---
+++ Basic-for-training/Loop-Based-21-45/22-NumbersDivisibleBy2And3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        if (i % 2 == 0 && i % 3 == 0)
+            printf("%d ", i);
+    }
+    return 0;
+}
