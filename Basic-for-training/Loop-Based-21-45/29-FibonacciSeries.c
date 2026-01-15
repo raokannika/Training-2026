d3325750d7216cf5be661236676b5034594b77fd
kannika07112 <kkannikarao07@gmail.com>
2026-01-15 16:05:31 +0530

    Question 29-updated solution

--- Basic-for-training/Loop-Based-21-45/29-FibonacciSeries.c
+++ Basic-for-training/Loop-Based-21-45/29-FibonacciSeries.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+int main() {
+    int i, n;
+    int t1 = 0, t2 = 1, nextTerm;
 
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Input should be greater than 0");
+        return 0;
+    }
+
+    if (n == 1) {
+        printf("Fibonacci Series: %d", t1);
+        return 0;
+    }
+
+    printf("Fibonacci Series: %d, %d", t1, t2);
+
+    for (i = 3; i <= n; i++) {
+        nextTerm = t1 + t2;
+        printf(", %d", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
+}
+/*
 int main() {
     int n, i;
     int a = 0, b = 1, c;
@@ -14,4 +42,4 @@ int main() {
         b = c;
     }
     return 0;
-}
+}*/
