6f8571024a9db6c89fd0f69a0c4ff24a7cc0928b
kannika07112 <kkannikarao07@gmail.com>
2026-01-16 19:25:32 +0530

    Question 59

---
+++ Basic-for-training/Patterns-46-60/59-IncreasingAlaphabet.c
@@ -0,0 +1,19 @@
+/*
+    A
+    AB
+    ABC
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Enter height: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 0; j < i; j++)
+            printf("%c", 'A' + j);
+        printf("\n");
+    }
+    return 0;
+}
