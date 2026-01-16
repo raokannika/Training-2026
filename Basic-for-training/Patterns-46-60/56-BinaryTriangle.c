00b286126239cf3128a00b4199d6cbb339fefd4f
kannika07112 <kkannikarao07@gmail.com>
2026-01-16 19:24:55 +0530

    Question 56

---
+++ Basic-for-training/Patterns-46-60/56-BinaryTriangle.c
@@ -0,0 +1,20 @@
+/*
+    1
+    0 1
+    1 0 1
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Enter rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++)
+            printf("%d ", (i + j) % 2);
+        printf("\n");
+    }
+    return 0;
+}
