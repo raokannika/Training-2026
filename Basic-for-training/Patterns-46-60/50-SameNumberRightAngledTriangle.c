cef5688a5a54a4bf29cbcf9280f1e491de7cb730
kannika07112 <kkannikarao07@gmail.com>
2026-01-16 19:22:50 +0530

    Question 50

---
+++ Basic-for-training/Patterns-46-60/50-SameNumberRightAngledTriangle.c
@@ -0,0 +1,20 @@
+/*
+    1
+    22
+    333
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Enter height: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++)
+            printf("%d", i);
+        printf("\n");
+    }
+    return 0;
+}
