972b52733469daab6950142b55b2251f8d662a1a
kannika07112 <kkannikarao07@gmail.com>
2026-01-18 21:03:26 +0530

    Question 83

---
+++ Basic-for-training/Strings-81-100/83-CountConsonant.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if ((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z')) {
+            if (!(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+                  str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'))
+                count++;
+        }
+    }
+
+    printf("Consonants count = %d", count);
+    return 0;
+}
