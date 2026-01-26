9dc199ae4fd803d6abf0c8d4d86f9de8c6203da5
kannika07112 <kkannikarao07@gmail.com>
2026-01-26 14:21:22 +0530

    program 1(2)

---
+++ DAY 4/withvalidationBill.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int quantity = 0;
+    int discountInt=0;
+    float price=0.0;
+    float bill=0.0;
+    float netAmount= 0.0;
+    float discount=0.0;
+    char name[100];
+    printf("Enter name of th customer: ");
+    scanf("%s",name);
+    printf("Enter Price: ");
+    scanf("%f",&price);
+    printf("Enter Quantity: ");
+    scanf("%d",&quantity);
+    if(quantity<=0)
+    {
+        printf("Invalid Quantity. Exiting.");
+    }
+    else{
+        bill= price * quantity;
+        if(bill>3000 && bill<=5000)
+        {
+            discountInt=10;
+            discount=bill*10/100;
+        }
+        else if(bill>5000)
+        {
+            discountInt=20;
+            discount=bill*20/100;
+        }
+        netAmount=bill-discount;
+        printf("\n------BILL------\n");
+        printf("Customer Name : %s \nTotal Bill    : Rs %.2f\nDiscount(%d%) : Rs %.2f\nNet Amount    : Rs %.2f\n",name,bill,discountInt,discount,netAmount);
+    }
+    
+    return 0;
+}
